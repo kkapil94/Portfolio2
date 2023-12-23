91bfe64629efb4ba517fd3398606722ddb8f7b18
kapil <khatrikapil987@gmail.com>
2023-12-23 09:01:24 +0530

    skills section updated

--- src/components/SkillSection.jsx
+++ src/components/SkillSection.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import front from "../utils/front.json";
 import back from "../utils/back.json";
-import other from "../utils/other.json";
+import devops from "../utils/devops.json";
 
 export default function SkillSection() {
   return (
@@ -47,12 +47,12 @@ export default function SkillSection() {
           </div>
           <div className="border-2 bg-[#0d1b2a] border-solid border-white rounded-2xl py-1">
             <h2 className="text-4xl my-4 text-center text-white font-bold">
-              Other
+              Devops
             </h2>
             <div className="h-[1px]  w-full bg-white"></div>
             <div className="px-4 my-8 flex flex-wrap items-center justify-center gap-4">
-              {other &&
-                other.map((item) => (
+              {devops &&
+                devops.map((item) => (
                   <div className="w-32 h-10 bg-gray-900 border-solid border-2 border-gray-600 flex items-center justify-between p-4 rounded-md">
                     <span className="text-md text-white">{item.name}</span>
                     <span>
